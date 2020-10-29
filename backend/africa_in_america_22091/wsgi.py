490eae1852dc306d08e6e8d118f63cac31c5c9c8
Crowdbotics <team@crowdbotics.com>
2020-10-29 17:17:43 +0000

    Update models

--- backend/africa_in_america_22091/wsgi.py
+++ backend/africa_in_america_22091/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'africa_in_america_22091.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "africa_in_america_22091.settings")
 
 application = get_wsgi_application()
